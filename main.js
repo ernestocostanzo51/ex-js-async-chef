2bd090547b999b72e6d0782b886696d960495cbf
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-06-15 19:21:45 +0200

    bugfix

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ async function fetchJson(url) {
     const obj = await response.json();
     return obj;
 }
-
-
 async function getChefBirthday(id) { 
     try{
          const ricetta = await fetchJson(`https://dummyjson.com/recipes/${id}`);
@@ -15,7 +13,6 @@ async function getChefBirthday(id) {
         throw error
     }
 }
-
 getChefBirthday(1)
   .then(birthday => console.log("Data di nascita dello chef:", birthday))
   .catch(error => console.error("Errore:", error.message));
